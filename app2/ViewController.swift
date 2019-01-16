0f570be1aaa1c5b194bf323d1cdfa62a635f37db
tops-bhadresh <tops.bhadresh@gmail.com>
2019-01-15 23:19:49 -0800

    upload

--- app2/ViewController.swift
+++ app2/ViewController.swift
@@ -10,10 +10,104 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtA: UITextField!
+    
+    @IBOutlet weak var txtB: UITextField!
+    
+    
+    @IBOutlet weak var lblAns: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func ChangeToPerformAction(_ sender: UISegmentedControl) {
+  
+    
+        if txtA.text != "" && txtB.text != ""{
+            
+        
+            let selectedIndex = sender.selectedSegmentIndex
+            
+            let a = Int32(txtA.text!)!
+            
+            let b = Int32(txtB.text!)!
+            
+            var c:Int32 = 0
+            
+            var ans:String = ""
+            
+            switch selectedIndex {
+                case 0:
+                    c = a + b
+                    lblAns.text = "\(c)"
+                    ans = "Addition of \(a) and \(b) is \(c)"
+                    break;
+                case 1:
+                    
+                    c = a - b
+                    ans = "Subtraction of \(a) and \(b) is \(c)"
+                    
+                    lblAns.text = "\(c)"
+                    break;
+                case 2:
+                    c = a / b
+                    
+                    ans = "Division of \(a) and \(b) is \(c)"
+                    lblAns.text = "\(c)"
+                    break;
+                case 3:
+                    
+                    c = a * b
+                    ans = "Multiplication of \(a) and \(b) is \(c)"
+                    lblAns.text = "\(c)"
+                    break;
+                default:
+                    lblAns.text = "N/A"
+                
+            }
+            
+            
+            let alert = UIAlertController(title: "Result", message: ans, preferredStyle: .alert)
+            
+            
+            let alertOkAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            
+            alert.addAction(alertOkAction)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            
+            
+        }
+        else{
+            
+            let alertActionSheet = UIAlertController(title: "Error", message: "Please Enter Numbers!", preferredStyle: .actionSheet)
+            
+            let alertCancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+                print("Cancel Action Performed!")
+            })
+            
+            alertActionSheet.addAction(alertCancelAction)
+            
+            self.present(alertActionSheet, animated: true, completion: nil)
+            
+            
+
+            
+            
+            
+        }
+    
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
